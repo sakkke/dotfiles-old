eed6e97350deb44193bc2d1e51d78d431e41e75b
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-22 07:43:21 +0000

    feat: set mapleader to space

--- src/linux/neovim/.config/nvim/init.lua
+++ src/linux/neovim/.config/nvim/init.lua
@@ -11,6 +11,8 @@ vim.opt.colorcolumn = '79'
 
 vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
 
+vim.g.mapleader = ' '
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
