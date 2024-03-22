c8dd3cee3e5a2c00fb1ddcc10bd2a113bf84c032
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-22 07:13:52 +0000

    feat: add which-key.nvim

--- src/linux/neovim/.config/nvim/init.lua
+++ src/linux/neovim/.config/nvim/init.lua
@@ -37,6 +37,19 @@ require("lazy").setup({
     end,
   },
   {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+  {
     'lewis6991/gitsigns.nvim',
     config = function()
       require('gitsigns').setup()
