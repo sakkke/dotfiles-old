6ef223f5feda791bc3bd7b33d2a0959425520dca
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-22 02:00:10 +0000

    feat: add condition to control hostname display

--- src/.profile.d/prompt.sh
+++ src/.profile.d/prompt.sh
@@ -1,1 +1,7 @@
-PS1='\[\033[1;32m\]\u@\h\[\033[m\]:\[\033[1;34m\]\w\[\033[m\]\$ '
+prompt='\[\033[1;34m\]\w\[\033[m\]\$ '
+
+if [ -n "$CONFIG_DISPLAY_HOSTNAME" ]; then
+  prompt='\[\033[1;32m\]\u@\h\[\033[m\]:'"$prompt"
+fi
+
+PS1="$prompt"
