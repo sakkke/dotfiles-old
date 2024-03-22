c8233bcbad6a344acdcb0dde20ba66bdce2f9d56
sakkke <84666033+sakkke@users.noreply.github.com>
2024-03-22 02:21:34 +0000

    refactor: refactor code

--- install.sh
+++ install.sh
@@ -2,7 +2,25 @@
 
 set -e
 
-(cd src && find -mindepth 1 -type d -print0) |
-  xargs -0I {} mkdir -p "$HOME/{}"
-(cd src && find -type f -print0 && find -type l -print0) |
-  xargs -0I {} ln -sfv "$PWD/src/{}" "$HOME/{}"
+abs_path() {
+  path="$1"
+
+  (cd `dirname "$path"` && echo "$PWD/`basename "$path"`")
+}
+
+link_profile() {
+  src_path="$1"
+
+  (cd "$src_path" && find -mindepth 1 -type d -print0) |
+    xargs -0I {} mkdir -p "$HOME/{}"
+  (cd "$src_path" && find -type f -print0 && find -type l -print0) |
+    xargs -0I {} ln -sfv "`abs_path "$src_path"`/{}" "$HOME/{}"
+}
+
+if [ "$#" -gt 0 ]; then
+  src_path="$1"
+else
+  src_path=src
+fi
+
+link_profile "$src_path"
